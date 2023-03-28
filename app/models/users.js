1c90876c3917a98f66360275c5bce6b021fbc5b6
cailudys <cailudys@gmail.com>
2023-03-28 14:35:54 +0800

    设计个人资料schema

--- app/models/users.js
+++ app/models/users.js
@@ -7,8 +7,34 @@ const { Schema, model } = mongoose
 const userSchema = new Schema({
     __v: { type: Number, select: false },
     name: { type: String, required: true },
-    password: { type: String, required: true, select: false }
+    password: { type: String, required: true, select: false },
+    // 图像存到数据库里的是图像的url
+    avatar_url: { type: String },
+    gender: { type: String, enum: ['male', 'female'], default: 'male', required: true },
+    headline: { type: String },
+    location: { type: [{ type: String }] },
+    business: { type: String },
+    employments: {
+        type: [
+            {
+                company: { type: String },
+                job: { type: String }
+            }
+        ]
+    },
+    educations: {
+        type: [{
+            school: { type: String },
+            major: { type: String },
+            diploma: { type: Number, enum: [1, 2, 3, 4, 5] },
+            entrance_year: { type: Number },
+            graduation_year: { type: Number },
+        }]
+    },
+
 })
 
-// 利用schema创建用户模型,返回的
+
+
+// 利用schema创建用户模型,返回的 
 module.exports = model('User', userSchema)
